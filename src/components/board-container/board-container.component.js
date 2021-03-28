fd3b6c4d9c005966fd86908187e528b1b0178b39
Harsh Pawar <pawarharsh21@gmail.com>
2021-03-28 19:02:08 +0530

    add board container component and css

--- src/components/board-container/board-container.component.js
+++ src/components/board-container/board-container.component.js
@@ -1,20 +1,31 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Board } from '../board/board.component';
 import './board-container.css';
 
-class BoardContainer extends Component {
+const BoardContainer = ({ location }) => {
 
-    constructor(props) {
-        super(props);
-    }
+    const [roomName, setRoomName] = useState('');
+    const [username, setUsername] = useState('');
+    const [userId, setUserId] = useState('');
 
-    render() {
-        return (
-            < div className="container" >
-                <Board></Board>
-            </div >
-        )
-    }
+    useEffect(() => {
+        const params = new URLSearchParams(location.search)
+        const roomName = params.get('roomName');
+        const username = params.get('username');
+        const userId = params.get('userId');
+        setUserId(userId);
+        setRoomName(roomName);
+        setUsername(username);
+    }, []);
+
+    return (
+        <>
+            {roomName && username &&
+                < div className="container" >
+                    <Board roomName={roomName} username={username} userId={userId} />
+                </div >}
+        </>
+    )
 }
 
 export { BoardContainer };
